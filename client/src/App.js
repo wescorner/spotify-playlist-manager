b44ab4ef3fd5838ba95c72c1def40ff40aa3f40c
Leeyanhawrt <leeyan1108@gmail.com>
2022-09-03 17:04:17 -0400

    Add exact to routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" exact element={<Login />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
       </Routes>
     </Router>
   );
