c88bfe22095658d47c3b86ba18342896bcfe6686
Leeyanhawrt <leeyan1108@gmail.com>
2022-09-03 17:00:46 -0400

    Add react router for login credentials

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+// import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <Container className="text-center mt-5">
-      <Login />
-    </Container>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
