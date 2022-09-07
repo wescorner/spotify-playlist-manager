6e92acd73cb3216cd5e1ad0f91fbc69d1b12ab06
Wesley Corner <wescorner@gmail.com>
2022-09-07 03:07:20 -0400

    profile button and styling changes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,15 @@ export default function Header() {
   return (
     <div className="header">
       <h1>Spotify Playlist Manager</h1>
-      <p>username</p>
+      <button id="profileButton">
+        <img
+          id="userPhoto"
+          src="https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt="profile"
+        />
+        <span id="username">freelancer5hire</span>
+      </button>
+      <button id="logoutButton">Logout</button>
     </div>
   );
 }
