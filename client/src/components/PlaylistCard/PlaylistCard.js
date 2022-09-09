e60bba2208e6ff6c16fe679d1e4a1db7cb741dde
Wesley Corner <wescorner@gmail.com>
2022-09-09 01:08:32 -0400

    add modal for adding playlists to category

--- client/src/components/PlaylistCard/PlaylistCard.js
+++ client/src/components/PlaylistCard/PlaylistCard.js
@@ -3,12 +3,6 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import "./PlaylistCard.scss";
 export default function PlaylistCard({ image, title, description, totalTracks, onClick }) {
   return (
-    // <Card onClick={onClick} className="bg-dark text-white playlistCard">
-    //   <Card.Img src={image} alt="Card image" />
-    //   <Card.Body>
-    //     <Card.Title>{title}</Card.Title>
-    //   </Card.Body>
-    // </Card>
     <div className="playlistCard">
       <img className="playlistIcon" src={image} alt="icon" />
       <p className="playlistName">{title}</p>
--- client/src/components/PlaylistsModal/PlaylistsModal.js
+++ client/src/components/PlaylistsModal/PlaylistsModal.js
@@ -1,10 +1,36 @@
 import Modal from "react-bootstrap/Modal";
+import PlaylistModalCard from "../PlaylistModalCard/PlaylistModalCard";
+import "./PlaylistModal.scss";
+
+const args = {
+  title: "Chill Vibes Playlist",
+  description: "A cool playlist for just vibing and jamming out",
+  totalTracks: 21,
+  image:
+    "https://www.liveabout.com/thmb/pwO4o_iDrZRTmmhs7eOfD25Qoqw=/1500x1125/smart/filters:no_upscale()/pop-music-57bce3863df78c87634ea806.jpg",
+};
 
 export default function PlaylistsModal(props) {
   return (
-    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Header closeButton>All Playlists</Modal.Header>
-      <Modal.Body>This is the text in the modal</Modal.Body>
+    <Modal {...props} size="lg" centered>
+      <Modal.Header closeButton>
+        <h3>All Playlists</h3>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="playlists">
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+          <PlaylistModalCard className="playlistItem" {...args} />
+        </div>
+      </Modal.Body>
     </Modal>
   );
 }
