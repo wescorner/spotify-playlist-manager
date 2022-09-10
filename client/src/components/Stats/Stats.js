a7fca5bd1658e701c2f1c2a76710694e215ace0b
Leeyanhawrt <leeyan1108@gmail.com>
2022-09-10 15:18:07 -0400

    Add changes to stats using state

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -13,7 +13,8 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import AudiotrackIcon from "@material-ui/icons/Audiotrack";
-import PersonIcon from "@material-ui/icons/Person"
+import ReplayIcon from '@material-ui/icons/Replay';
+import axios from "axios";
 
 ChartJS.register(
   CategoryScale,
@@ -24,60 +25,80 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      labels: {
-        display: false,
-        color: "white",
-        font: {
-          size: 18
+export default function Stats() {
+
+  const [songs, setSongs] = useState([])
+  const [playCounts, setPlayCounts] = useState([])
+  const [popular, setPopular] = useState([])
+
+  useEffect(() => {
+    const songList = [];
+    const playCountList = [];
+    const popularList = [];
+    axios.get(`/playlist/10`)
+      .then(res => {
+        console.log(res.data)
+        res.data.forEach(track => {
+          songList.push(track.name)
+          setSongs(songList)
+          playCountList.push(track.playCount)
+          setPlayCounts(playCountList)
+          setPopular([res.data[0].name, res.data[0].playCount])
+        })
+      })
+  }, [])
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        labels: {
+          display: false,
+          color: "white",
+          font: {
+            size: 18
+          }
         }
       }
-    }
-  },
-  scales: {
-    y: {
-      ticks: {
-        color: "grey",
-        font: {
-          size: 15,
-        },
-        stepSize: 1,
-        beginAtZero: true
+    },
+    scales: {
+      y: {
+        ticks: {
+          color: "grey",
+          font: {
+            size: 15,
+          },
+          stepSize: 1,
+          beginAtZero: true
+        }
+      },
+      x: {
+        ticks: {
+          color: "grey",
+          font: {
+            size: 20
+          },
+          stepSize: 1,
+          beginAtZero: true
+        }
       }
     },
-    x: {
-      ticks: {
-        color: "grey",
-        font: {
-          size: 20
-        },
-        stepSize: 1,
-        beginAtZero: true
-      }
-    }
-  },
-};
+  };
 
-const labels = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5', 'Song 6', 'Song 7',];
+  const labels = songs;
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Number of plays per song',
-      data: [535, 432, 421, 327, 315, 245, 244],
-      backgroundColor: 'rgba(30, 215, 96, 0.5)',
-    },
-  ],
-};
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Number of plays per song',
+        data: playCounts,
+        backgroundColor: 'rgba(30, 215, 96, 0.5)',
+      },
+    ],
+  };
 
-
-export default function Stats() {
   return (
-
     <div className="App">
       <div className="navbar">
         <Navbar />
@@ -89,9 +110,9 @@ export default function Stats() {
             <h1 className='playlistTitle'>Playlist Name</h1>
             <img id="playlistImage" src='https://thisis-images.scdn.co/37i9dQZF1DZ06evO4vfeYm-large.jpg' alt="playlistImage" />
           </div>
-          <div class="flexColumn">
-            <p><AudiotrackIcon className="svg_icons" />Most Played Song:</p>
-            <p><PersonIcon className="svg_icons" />Most Popular Artist:</p>
+          <div className="flexColumn">
+            <p><AudiotrackIcon className="svg_icons" />Most Played Song: {popular[0]} </p>
+            <p><ReplayIcon className="svg_icons" />Number of Listens: {popular[1]}</p>
           </div>
         </div>
         <div id="buttonContainer">
