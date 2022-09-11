7327650af993ba3dc6bdab4ee57c63e4cb7de1a8
Leeyanhawrt <leeyan1108@gmail.com>
2022-09-11 14:19:38 -0400

    Complete stats page graphic

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -12,8 +12,10 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+import { useParams } from "react-router-dom"
 import AudiotrackIcon from "@material-ui/icons/Audiotrack";
-import PersonIcon from "@material-ui/icons/Person"
+import ReplayIcon from '@material-ui/icons/Replay';
+import axios from "axios";
 
 ChartJS.register(
   CategoryScale,
@@ -24,58 +26,82 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      labels: {
-        display: false,
-        color: "white",
-        font: {
-          size: 18
+export default function Stats() {
+
+  const { id } = useParams()
+  const [dashboardStats, setDashboardStats] = useState({})
+  const [favourite, setFavourite] = useState([])
+  const [playlistInfo, setPlaylistInfo] = useState([])
+
+  useEffect(() => {
+    const songList = [];
+    const playCountList = [];
+    axios.get(`/playlist/${id}`)
+      .then(res => {
+        setPlaylistInfo([res.data.name, res.data.image])
+        res.data.tracks.forEach(track => {
+          songList.push(track.name)
+          playCountList.push(track.playCount)
+        })
+        setFavourite([res.data.tracks[0].name, res.data.tracks[0].playCount])
+        setDashboardStats(prev => ({
+          ...prev, songList, playCountList
+        }))
+      })
+  }, [])
+
+  console.log(dashboardStats)
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        labels: {
+          display: false,
+          color: "white",
+          font: {
+            size: 18
+          }
         }
       }
-    }
-  },
-  scales: {
-    y: {
-      ticks: {
-        color: "grey",
-        font: {
-          size: 15,
-        },
-        stepSize: 1,
-        beginAtZero: true
+    },
+    scales: {
+      y: {
+        ticks: {
+          color: "grey",
+          font: {
+            size: 15,
+          },
+          stepSize: 1,
+          beginAtZero: true
+        }
+      },
+      x: {
+        ticks: {
+          color: "grey",
+          font: {
+            size: 20
+          },
+          stepSize: 1,
+          beginAtZero: true
+        }
       }
     },
-    x: {
-      ticks: {
-        color: "grey",
-        font: {
-          size: 20
-        },
-        stepSize: 1,
-        beginAtZero: true
-      }
-    }
-  },
-};
+  };
 
-const labels = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5', 'Song 6', 'Song 7',];
+  const labels = dashboardStats.songList;
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Number of plays per song',
-      data: [535, 432, 421, 327, 315, 245, 244],
-      backgroundColor: 'rgba(30, 215, 96, 0.5)',
-    },
-  ],
-};
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Number of plays per song',
+        data: dashboardStats.playCountList,
+        backgroundColor: 'rgba(30, 215, 96, 0.5)',
+      },
+    ],
+  };
 
-
-export default function Stats() {
   return (
 
     <div className="App">
@@ -86,12 +112,12 @@ export default function Stats() {
         <Header />
         <div className="statsHeader">
           <div>
-            <h1 className='playlistTitle'>Playlist Name</h1>
-            <img id="playlistImage" src='https://thisis-images.scdn.co/37i9dQZF1DZ06evO4vfeYm-large.jpg' alt="playlistImage" />
+            <h1 className='playlistTitle'>{playlistInfo[0]}</h1>
+            <img id="playlistImage" src={playlistInfo[1]} alt="playlistImage" />
           </div>
-          <div class="flexColumn">
-            <p><AudiotrackIcon className="svg_icons" />Most Played Song:</p>
-            <p><PersonIcon className="svg_icons" />Most Popular Artist:</p>
+          <div className="flexColumn">
+            <p><AudiotrackIcon className="svg_icons" />Most Played Song: {favourite[0]}</p>
+            <p><ReplayIcon className="svg_icons" />Number of Plays: {favourite[1]}</p>
           </div>
         </div>
         <div id="buttonContainer">
